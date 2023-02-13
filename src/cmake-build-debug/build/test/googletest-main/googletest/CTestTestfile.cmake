60bedea7a3bdfe90e9e3c265d7c35f3d8c34a6d5
borhez <getlink32@gmail.com>
2023-02-13 13:53:43 +0300

    clear build-folder; Для сборки, захожу в папку build и запускаю  cmake ../..

--- src/cmake-build-debug/build/test/googletest-main/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sshield/projects/sber/navigator/repos (копия)/src/test/googletest-main/googletest
-# Build directory: /home/sshield/projects/sber/navigator/repos (копия)/src/cmake-build-debug/build/test/googletest-main/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
