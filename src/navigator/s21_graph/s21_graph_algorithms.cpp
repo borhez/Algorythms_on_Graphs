6094af53087f1bb7859bc83bf5c630cf48ab3ce3
borhez <getlink32@gmail.com>
2023-02-15 15:57:15 +0300

    wrong cycle

--- src/navigator/s21_graph/s21_graph_algorithms.cpp
+++ src/navigator/s21_graph/s21_graph_algorithms.cpp
@@ -1,4 +1,5 @@
 #include "s21_graph_algorithms.h"
+#include <algorithm>
 std::vector<int> GraphAlgorithms::depthFirstSearch(Graph &graph, int startVertex){
   int iRes = 0;
   std::vector<int> res(graph.getVerticesNumber(), 0);
@@ -112,9 +113,10 @@ void printMatrix2dVector(std::vector<std::vector<int>>& mtrx, size_t size)
 		}
 		printf("\n");
 	}
+	printf("\n");
 }
 
-void printVectorElements(std::vector<int> vec)
+void printVectorElements(std::vector<int>& vec)
 {
 	printf("\nVector:");
 	for (size_t i=0; i < vec.size(); i++)
@@ -140,19 +142,32 @@ int	findStartVert(std::vector<std::vector<int>>& helpMatrix, size_t nVertices)
 				return row;
 		}
 	}
+	return (-1);
 }
+
+bool checkAllElemsAreZero(std::vector<int> v)
+{
+	for (size_t i = 0; i < v.size(); i++)
+	{
+		if (v[i] != 0)
+			return (0);
+	}
+	return (1);
+}
+
+
 
 std::vector<std::vector<int>> GraphAlgorithms::getLeastSpanningTree(Graph &graph)
 {
 	printf("in func_getLeastSpanningTree(Graph &graph)\n");
 	size_t nVertices = graph.getVerticesNumber();
-	std::vector<std::vector<int>> mtrxOstTree(nVertices);
+	std::vector<std::vector<int>> mtrxOstovTree(nVertices);
 	std::vector<std::vector<int>> helpMatrix(nVertices);
 	
 	for (size_t i = 0; i < nVertices; i++)
 	{
-		mtrxOstTree[i].reserve(nVertices);
-		helpMatrix[i].reserve(nVertices);
+		mtrxOstovTree[i].resize(nVertices);
+		helpMatrix[i].resize(nVertices);
 	}
 	
 
@@ -165,17 +180,17 @@ std::vector<std::vector<int>> GraphAlgorithms::getLeastSpanningTree(Graph &graph
 			if (i != j)
 				helpMatrix[i][j] = graph.getMatrxElem(i, j);
 			else { helpMatrix[i][j] = 0; }
-			mtrxOstTree[i][j] = 0;
+			mtrxOstovTree[i][j] = 0;
 		}
 	}
-	printMatrix2dVector(helpMatrix, nVertices);
+	// printf("обнулил диагональные элементы\n");
+	// printMatrix2dVector(helpMatrix, nVertices);
 
 
 
 	/*	2 vectors для хранения вершин. На каждом шаге буду добавлять
 		и убавлять вершину из этих векторов, при построении остова дерева
 	*/
-	int	minWeight;
 	std::vector<int> unconnectedVert(nVertices);
 	std::vector<int> connectedVert(0);
 	for (size_t i = 0; i < nVertices; i++)
@@ -191,18 +206,55 @@ std::vector<std::vector<int>> GraphAlgorithms::getLeastSpanningTree(Graph &graph
 	int startVert = findStartVert(helpMatrix, nVertices);
 	// printf("%d\n", helpMatrix[startVert][3]);
 	
-
+	//удаляю стартовую вершину из unconnected, добавляю в connected
+	remove(unconnectedVert, startVert);
+	connectedVert.push_back(startVert);
+	// printVectorElements(unconnectedVert);
 
 
 
 	//Search ostovTree from the 1st vertex
 	//Stop search when all vertices will be connected;
-	// while (unconnectedVert.size() > 0)
-	// {
+	int foundVert, tempRow, tempCol, iTemp;
+	int n = 0;
+	int	minWeight;
+	while (n < nVertices)
+	{
+			printMatrix2dVector(helpMatrix, nVertices);
+			printVectorElements(connectedVert);
+
+		minWeight = std::numeric_limits<int>::max();
+		for (size_t i = 0; i < connectedVert.size(); i++)
+		{
+			int row = connectedVert.at(i);
+			for (size_t col = 0; col < nVertices; col++)
+			{
+				if (helpMatrix[row][col] > 0 && helpMatrix[row][col] < minWeight)
+				{
+					minWeight = helpMatrix[row][col];
+					tempRow=row;
+					tempCol=col;
+					iTemp = i;
+				}
+			}
+		}
+		printf("rowTemp=%d; colTemp=%d; iTemp=%d\n", tempRow, tempCol, iTemp);
+		mtrxOstovTree[tempRow][tempCol] = helpMatrix[tempRow][tempCol];
+		helpMatrix[tempRow][tempCol] = 0;
+		helpMatrix[tempCol][tempRow] = 0;
+				printf("\nafter edit");
+		printVectorElements(helpMatrix[tempRow]);
+
+		if (checkAllElemsAreZero(helpMatrix[tempRow]))
+			remove(connectedVert, iTemp);
+		printVectorElements(connectedVert);
+		if (!checkAllElemsAreZero(helpMatrix[tempCol]))
+			connectedVert.push_back(tempCol);
+		printVectorElements(connectedVert);
+		n++;
+
+	}
 
 
-	// }
-
-
-	return (mtrxOstTree);
+	return (mtrxOstovTree);
 }
